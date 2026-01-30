c8e8b33f95d078e8cdb42669e95a393378dc1388
Khizer132 <khixerimtiaz@gmail.com>
2026-01-30 16:29:54 +0500

    Deployemnet issue fixes

--- src/index.js
+++ src/index.js
@@ -36,6 +36,8 @@ app.use("/api/payment", paymentRoutes);
 connectDB();
 
 
-app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT);
-});
+// app.listen(PORT, () => {
+//     console.log('Server is running on port ' + PORT);
+// });
+
+module.exports = app;
