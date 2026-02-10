02d50ecf43972386c879f56cfce606463c082bc8
Khizer132 <khixerimtiaz@gmail.com>
2026-02-10 13:18:52 +0500

    minimal change

--- src/index.js
+++ src/index.js
@@ -40,8 +40,8 @@ app.use("/api/payment", paymentRoutes);
 connectDB();
 
 
-app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT);
-});
+// app.listen(PORT, () => {
+//     console.log('Server is running on port ' + PORT);
+// });
 
-// export default app;
+export default app;
